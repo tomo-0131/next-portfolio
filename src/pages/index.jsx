c083deda06072bcc8a177b25b1c9538bdc697cbc
tomo-0131 <woovoodoor@yahoo.co.jp>
2021-12-26 22:07:11 +0900

    fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,10 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel, HStack, Box, Text } from '@cha
 import Link from 'next/link';
 
 export default function Home() {
-  const profile = JSON.parse([{ name: 'Tomoyuki Kumagai' }, { location: 'Aichi' }]);
-
   return (
     <Box>
       <Text fontSize='2xl' align='center' justify='center' mt='2rem'>
-        {profile.map(({ name, location }, i) => {
-          return { name };
-        })}
+        Tomoyuki Kumagai
       </Text>
     </Box>
   );
